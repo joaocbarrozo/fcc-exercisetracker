1819279fdd7dd466b93808bd6e2c5feb77d53ab6
joaocbarrozo <97959049+joaocbarrozo@users.noreply.github.com>
2025-02-11 11:48:34 -0300

    Update index.js post api/users/:_id/exercises endpoint  with comments

--- index.js
+++ index.js
@@ -54,12 +54,17 @@ app.get('/api/users', (req, res) => {
 
 // Post exercises endpoint
 app.post('/api/users/:_id/exercises', (req, res) => {
+  // Get id from parameters
   const { _id } = req.params;
+  // Find user wit the input id
   const username = users.find(user => user._id === _id).username;
   if (username) {
     const { description, duration, date } = req.body;
-    const exerciseDuration = parseInt(duration)
+    // Cast duration to INT
+    const exerciseDuration = parseInt(duration);
+    // Format date
     const exerciseDate = date ? new Date(date).toDateString(): new Date().toDateString();
+    // Create object newExercise
     const newExercise = { 
       'username': username,
       'description': description, 
@@ -67,15 +72,20 @@ app.post('/api/users/:_id/exercises', (req, res) => {
       'date': exerciseDate,
       'id': _id
     };
+    // Add newExercise to exercises array
     exercises.push(newExercise);
+    // Create object newLog
     const newLog = {
       'description': description, 
       'duration': exerciseDuration, 
       'date': exerciseDate
     }
+    // Find the user in log array
     const userLog = log.find(user => user._id === _id);
+    // Update user log and count
     userLog.log.push(newLog);
     userLog.count++;
+    // Response JSON with the user and exercise added
     res.json({
       '_id': _id,
       'username': username,
